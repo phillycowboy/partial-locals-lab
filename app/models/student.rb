3937f19bf9236b575857954e953423f0c31740d1
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-01-25 15:20:28 -0500

    commit tree d0baf59092f94e0ad0b0fc8737b52e613901bbb0

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,8 +9,21 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+require 'pry'
 
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  validates :name, presence: true 
+  validates :hometown, presence: true 
+  validates :birthday, presence: true 
+
+  def self.search(query)
+    if query.present?
+      # binding.pry
+      where('NAME like ?', "%#{query}%")
+    else
+      self.all
+    end
+  end
 end
